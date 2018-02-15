dd26ee304555f01c695733dbf391d51c5dc53cbb
stathismast <Stathis@DESKTOP-C42DOTC.localdomain>
2018-02-15 23:39:40 +0200

    Expanded on finalOnTheWay.c, it now has every process apply the filter in every pixel except the 'edge' pixels.

--- test2Darray/test.c
+++ test2Darray/test.c
@@ -23,6 +23,7 @@ int main(void){
     int cols = 6;
     int ** array = get2DArray(rows,cols);
 
+    int (*array2D) [cols] = malloc( sizeof(int[rows][cols]) );
 
     for(int i=0; i<rows; i++){
         for(int j=0; j<cols; j++){
@@ -32,11 +33,26 @@ int main(void){
 
     for(int i=0; i<rows; i++){
         for(int j=0; j<cols; j++){
-            printf("%d", array[i][j]);
+            array2D[i][j] = i+j;
         }
-        printf("\n");
     }
 
+    for(int i=0; i<rows; i++){
+       for(int j=0; j<cols; j++){
+           printf("%d", array[i][j]);
+       }
+       printf("\n");
+    }
+
+    for(int i=0; i<rows; i++){
+       for(int j=0; j<cols; j++){
+           printf("%d", array2D[i][j]);
+       }
+       printf("\n");
+    }
+
+    printf("\nMy first pointer  : %d\nMy second pointer : %d\n\n", (unsigned int)array[0], (unsigned int)array[1]);
+    printf("\nTheir first pointer  : %d\nTheir second pointer : %d\n\n", (unsigned int)array2D[0], (unsigned int)array2D[1]);
 
     free2DArray(array);
 }
